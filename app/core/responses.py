8e4c859c5ceac18bcf1d0ad27d48f2333d9be09c
Crissancio <soyelpapu1509@gmail.com>
2026-05-27 18:55:06 -0400

    eliminacion de espacio

--- app/core/responses.py
+++ app/core/responses.py
@@ -1,16 +1,11 @@
 from typing import Generic, List, TypeVar
-
 from pydantic import BaseModel
-
-
 T = TypeVar("T")
-
 
 class ResponseBase(BaseModel, Generic[T]):
     success: bool = True
     data: T
     message: str
-
 
 class PaginationMeta(BaseModel):
     page: int
@@ -18,11 +13,9 @@ class PaginationMeta(BaseModel):
     total: int
     total_pages: int
 
-
 class PaginatedData(BaseModel, Generic[T]):
     items: List[T]
     meta: PaginationMeta
-
 
 class PaginatedResponse(ResponseBase[PaginatedData[T]]):
     pass
